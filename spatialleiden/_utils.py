c9f2e7a9fc4a2b7210e037cf6ce2dff752b8b1b9
niklasmueboe <sputnik.mb@web.de>
2024-05-29 10:39:38 +0200

    remove unused alias

--- spatialleiden/_utils.py
+++ spatialleiden/_utils.py
@@ -1,9 +1,4 @@
-from typing import TypeAlias
-
-import numpy as np
 from scipy.sparse import csr_array, sparray, spmatrix
-
-_GraphArray: TypeAlias = sparray | spmatrix | np.ndarray
 
 
 def distance2connectivity(distances: sparray | spmatrix) -> sparray | spmatrix:
